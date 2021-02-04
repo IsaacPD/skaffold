0421af97cf8bff10e631b343dff61cbdb940b0d1
Isaac Duarte <isaacpd@google.com>
2021-02-04 12:23:00 -0500

    use status code string in error label

--- pkg/skaffold/instrumentation/export_test.go
+++ pkg/skaffold/instrumentation/export_test.go
@@ -23,7 +23,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -240,7 +239,7 @@ func checkOutput(t *testutil.T, meters []skaffoldMeter, b []byte) {
 		durationCount[fmt.Sprintf("%s:%f", meter.Command, meter.Duration.Seconds())]++
 		archCount[meter.Arch]++
 		commandCount[meter.Command]++
-		errorCount[meter.ErrorCode]++
+		errorCount[meter.ErrorCode.String()]++
 
 		for k, v := range meter.EnumFlags {
 			n := FlagsPrefix + strings.ReplaceAll(k, "-", "_")
@@ -277,9 +276,9 @@ func checkOutput(t *testutil.T, meters []skaffoldMeter, b []byte) {
 			archCount[l.Labels["arch"]]--
 			osCount[l.Labels["os"]]--
 			versionCount[l.Labels["version"]]--
-			e, _ := strconv.Atoi(l.Labels["error"])
-			if e == int(proto.StatusCode_OK) {
-				errorCount[proto.StatusCode(e)]--
+			e := l.Labels["error"]
+			if e == proto.StatusCode_OK.String() {
+				errorCount[e]--
 			}
 		case "launch/duration":
 			durationCount[fmt.Sprintf("%s:%f", l.Labels["command"], l.value().(float64))]--
@@ -292,11 +291,11 @@ func checkOutput(t *testutil.T, meters []skaffoldMeter, b []byte) {
 		case "deployer":
 			deployers[l.Labels["deployer"]]--
 		case "dev/iterations":
-			e, _ := strconv.Atoi(l.Labels["error"])
-			devIterations[devIteration{l.Labels["intent"], proto.StatusCode(e)}]--
+			e := l.Labels["error"]
+			devIterations[devIteration{l.Labels["intent"], proto.StatusCode(proto.StatusCode_value[e])}]--
 		case "errors":
-			e, _ := strconv.Atoi(l.Labels["error"])
-			errorCount[proto.StatusCode(e)]--
+			e := l.Labels["error"]
+			errorCount[e]--
 		default:
 			switch {
 			case meteredCommands.Contains(l.Name):
